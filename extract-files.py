187c09b7260ab4b05808df368fdfe796963cba17
dianlujitao <dianlujitao@lineageos.org>
2022-10-07 20:29:49 +0800

    sky: Nuke ATFWD
    It's normally not used at all.
    
    Change-Id: Idb824b3b8c783b6ac962e604df05f9154beb4ed2

--- extract-files.py
+++ extract-files.py
@@ -110,8 +110,6 @@ blob_fixups: blob_fixups_user_type = {
         ),
     'vendor/etc/media_codecs_ravelin.xml': blob_fixup()
         .regex_replace('.+media_codecs_(google_audio|google_c2|google_telephony|vendor_audio).+\n', ''),
-    'vendor/etc/seccomp_policy/atfwd@2.0.policy': blob_fixup()
-        .add_line_if_missing('gettid: 1'),
     'vendor/etc/seccomp_policy/wfdhdcphalservice.policy': blob_fixup()
         .add_line_if_missing('gettid: 1'),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
