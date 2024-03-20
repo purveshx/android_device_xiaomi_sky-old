2a522843c799feb56fc984fb47aa12e22b35aa21
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    sky: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- device.mk
+++ device.mk
@@ -18,9 +18,6 @@ $(call inherit-product, vendor/xiaomi/sky/sky-vendor.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
-# Enable updating of APEXes.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Project ID Quota.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
