66f0e23ed9a2b56dbe49b83f4b9415faa089771c
Arian <arian.kulmer@web.de>
2025-02-18 18:00:35 +0100

    sky: Generate qcrilNr.db during build
    Change-Id: I1f3425713b9b5b1cc7185bc94124b9f01efe3abb

--- device.mk
+++ device.mk
@@ -413,6 +413,9 @@ PRODUCT_PACKAGES += \
     telephony-ext \
     xiaomi-telephony-stub
 
+PRODUCT_PACKAGES += \
+    qcrilNrDb_vendor
+
 PRODUCT_BOOT_JARS += \
     telephony-ext \
     xiaomi-telephony-stub
