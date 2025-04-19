5573706751e2c3e296c162eb16d7d9f9df8b0ea0
Lostark13 <tushar9915@gmail.com>
2025-04-19 09:47:46 +0000

    sky: LineageOS

--- lineage_sky.mk
+++ lineage_sky.mk
@@ -13,9 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit common Bliss configurations
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/bliss/config/BoardConfigReservedSize.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigReservedSize.mk)
 
 # Inherit device configurations
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
@@ -27,7 +27,7 @@ BLISS_BUILDTYPE := OFFICIAL
 
 ## Device identifier
 PRODUCT_DEVICE := sky
-PRODUCT_NAME := bliss_sky
+PRODUCT_NAME := lineage_sky
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 23076RN4BI
 PRODUCT_MANUFACTURER := Xiaomi
